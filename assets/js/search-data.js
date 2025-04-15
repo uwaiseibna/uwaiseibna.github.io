bd0a309af6e5535395b3edebc39d49e113dccab7
uwaiseibna <34473452+uwaiseibna@users.noreply.github.com>
2025-04-15 05:18:30 +0000

    Deploying to gh-pages from @ uwaiseibna/uwaiseibna.github.io@4a979cc624b860098957f962b02001dc80f1da76 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,11 +28,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-gap-is-now-out-on-briefings-in-bioinformatics-5yrs-if-13-99",
           title: 'GAP is now out on Briefings in Bioinformatics (5yrs IF: 13.99) 🥳',
           description: "",
